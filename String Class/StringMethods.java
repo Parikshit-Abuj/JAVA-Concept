5ae938a8a3d46c65167b32348623b63b9442a036
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-27 10:32:57 +0530

    Create StringMethods.java

---
+++ String Class/StringMethods.java
@@ -0,0 +1,31 @@
+package stringpractice1;
+
+public class StringPractice1 {
+
+    public static void main(String[] args) {
+
+        String str = new String("netbeans");
+
+        int len = str.length();
+        System.out.println(len);
+
+        String strUpper = str.toUpperCase();
+        System.out.println(strUpper);
+
+        str = str.trim();
+        System.out.println(str);
+
+        String strSub = str.substring(3);
+        System.out.println(strSub);
+
+        String str2 = str.replace('e', 'M');
+        System.out.println(str2 + " " + str);
+    }
+}
+
+OUTPUT:
+8
+NETBEANS
+netbeans
+beans
+nMtbMans netbeans
