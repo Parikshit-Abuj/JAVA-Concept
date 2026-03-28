e4f04fe0bdce9cf977e38a4152761c6086234851
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-28 11:25:00 +0530

    Update StringProblem.java

--- String Class/StringProblem.java
+++ String Class/StringProblem.java
@@ -7,12 +7,12 @@ public class StringProblem {
         for (String str : emails) {
             int i = str.indexOf("@");
             String uname = str.substring(0, i);
-            String domain = str.substring(i + 1, str.length());
+            String domain = str.substring(i + 1);
 
             System.out.println("Email: " + str);
             System.out.println("Username :" + uname);
             System.out.println("Domain :" + domain);
-            System.out.println(name.equals("gmail"));
+            System.out.println(domain.startsWith("gmail"));
             System.out.println();
         }
     }
