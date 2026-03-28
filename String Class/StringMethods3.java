7853b8fa07eeef2be41221d53b547a31a15d18f8
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-28 10:29:23 +0530

    Create StringMethods3.java

---
+++ String Class/StringMethods3.java
@@ -0,0 +1,23 @@
+public class StringMethods3 {
+
+    public static void main(String[] args) {
+
+        String a1 = "Pyramid";
+        String a2 = "pyramid";
+
+        System.out.println(a1.equals(a2));
+        System.out.println(a1.equalsIgnoreCase(a2));
+
+        String b1 = "china wall";
+        String b2 = new String("china tall");
+
+        System.out.println(b1.compareTo(b2));
+
+        String c1 = "the great wall ";
+        String c2 = "of china";
+
+        System.out.println(c1.contains("wall"));
+        System.out.println(c1.concat(c2));
+
+    }
+}
