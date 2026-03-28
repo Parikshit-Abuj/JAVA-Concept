eba17090187f44bb1400622386bf49c2623f1208
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-28 10:49:12 +0530

    Create RegularExpression.java

---
+++ String Class/RegularExpression.java
@@ -0,0 +1,42 @@
+package stringpractice4;
+
+public class StringPractice4 {
+
+    public static void main(String[] args) {
+
+        String str1 = "f";
+        System.out.println(str1.matches("."));
+
+        String str2 = "a";
+        System.out.println(str2.matches("[abc]"));
+
+        String str3 = "p";
+        System.out.println(str3.matches("[^abc]"));
+
+        String str4 = "7";
+        System.out.println(str4.matches("[a-zA-Z0-9]"));
+
+        String str5 = "b";
+        System.out.println(str5.matches("a|b"));
+
+        String str6 = "b";
+        System.out.println(str6.matches("\\w"));
+
+        String str7 = "5";
+        System.out.println(str7.matches("\\d"));
+
+        String str8 = "$";
+        System.out.println(str8.matches("\\D"));
+
+    }
+}
+
+OUTPUT
+true
+true
+true
+true
+true
+true
+true
+true
