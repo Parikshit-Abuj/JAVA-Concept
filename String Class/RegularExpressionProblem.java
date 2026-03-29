e53f597ed99a4ac518379453edc2eecf204d5d98
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-29 16:56:48 +0530

    Create RegularExpressionProblem.java
    Added methods to check binary strings, remove special characters, and count words in a string.

---
+++ String Class/RegularExpressionProblem.java
@@ -0,0 +1,29 @@
+package scstring;
+
+public class RegularExpressionProblem {
+
+    static boolean isBinary(String str) {
+        return str.matches("[01]+");
+    }
+
+    static String removeSpecial(String str) {
+        return str.replaceAll("[^a-zA-Z0-9]", "");
+    }
+
+    static int countWords(String str) {
+        str = str.replaceAll("\\s+", " ").trim();
+        return str.split("\\s").length;
+    }
+
+    public static void main(String[] args) {
+
+        String binary = "100110010";
+        System.out.println(isBinary(binary));
+
+        String special = "a!B@c#1$2%3";
+        System.out.println(removeSpecial(special));
+
+        String sentence = "      abc     def     gh   ijk    ";
+        System.out.println(countWords(sentence));
+    }
+}
