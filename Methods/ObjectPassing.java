f7ed8d4c3b657fcf4f445c52797a26cbbc4940b6
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-04-05 10:13:05 +0530

    Create ObjectPassing.java
    Demonstrate object and primitive passing in Java.

---
+++ Methods/ObjectPassing.java
@@ -0,0 +1,41 @@
+public class ObjectPassing {
+
+    static void change(int A[], int index, int value)
+    {
+        A[index] = value;
+    }
+
+    static void change2(int x, int value)
+    {
+        x = value;
+    }
+
+    public static void main(String[] args) {
+
+        int A[] = {2,4,6,8,10};
+
+        change(A, 2, 20);
+
+        for(int x : A)
+        {
+            System.out.println(x);
+        }
+
+        System.out.println();
+
+        int x = 10;
+
+        change2(x, 20);
+
+        System.out.println("Value of primitive " + x);
+    }
+}
+
+OUTPUT
+2
+4
+20
+8
+10
+
+Value of primitive 10
