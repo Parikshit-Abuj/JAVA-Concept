c9fa498171a7f6d956ba6063651a4dbc3df93622
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-04-13 09:18:42 +0530

    Sub method cant be private, Sup method can be private

---
+++ OOP/polymorphism/Overriderules.java
@@ -0,0 +1,39 @@
+package overriderules;
+
+class A {}
+
+class B extends A {}
+
+class Super
+{
+    public A display()
+    {
+        System.out.println("Super Display");
+        return new A();
+    }
+}
+
+class Sub extends Super
+{
+    @Override
+    public B display()
+    {
+        System.out.println("Sub Display");
+        return new B();
+    }
+}
+
+public class OverrideRules
+{
+    public static void main(String[] args)
+    {
+        Super s = new Sub();
+        A obj = s.display();
+
+        System.out.println("Returned object: " + obj.getClass().getSimpleName());
+    }
+}
+
+OUTPUT
+Sub Display
+Returned object: B
